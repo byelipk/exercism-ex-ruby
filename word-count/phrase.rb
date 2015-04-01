e7c70c424d96476b89aae4d049310fc0155fbe6c
byelipk <byelipk@gmail.com>
2015-04-01 18:40:33 -0400

    use reduce in word count

--- word-count/phrase.rb
+++ word-count/phrase.rb
@@ -4,7 +4,6 @@ class Phrase
 
   def initialize(text)
     @text = text
-    @frequency = Hash.new(0)
   end
 
   def word_count
@@ -14,11 +13,10 @@ class Phrase
   private
 
   def generate_histogram
-    text.scan(word_definition).each do |word|
-      frequency[word.downcase] += 1
+    text.scan(word_definition).reduce(Hash.new(0)) do |freq, word|
+      freq[word.downcase] += 1
+      freq
     end
-
-    frequency
   end
 
   def word_definition
